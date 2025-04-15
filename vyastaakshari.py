41a60a8606090ab660ae6e152be532dcba7d01ef
Vijaya Sagar Vinnakota <vijayasagar@gmail.com>
2025-04-15 15:23:40 +0530

    added args

--- vyastaakshari.py
+++ vyastaakshari.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import regex
 import random
-
-N = 16
 
 # ----
 # https://stackoverflow.com/questions/33068727/how-to-split-unicode-strings-character-by-character-in-python
@@ -16,7 +15,14 @@ N = 16
 # 
 # ----
 
-f = open("./vyastakshari_input.txt", "r", encoding="utf-8")
+if len(sys.argv) < 3:
+    print("usage: ", sys.argv[0], " <input_filename> <max_aksharams>")
+    exit(-1)
+
+inp_f = sys.argv[1]
+maxlen = sys.argv[2]
+
+f = open(inp_f, "r", encoding="utf-8")
 lines = f.readlines()
 n_lines = len(lines)
 r = random.randint(0, n_lines-1)
@@ -28,9 +34,9 @@ chars = regex.findall(r'\X', l)
 n = len(chars)
 print("len = ", n)
 
-N = min(n, N)
+maxlen = min(n, maxlen)
 
-random_order = random.sample(range(0, N), N)
+random_order = random.sample(range(0, maxlen), maxlen)
 print(random_order)
 
 for i in random_order:
@@ -40,14 +46,14 @@ for i in random_order:
 input("Press enter to start giving the correct order")
 
 new_l = ""
-for i in range(0, N):
+for i in range(0, maxlen):
     new_l = new_l + input()
 
 print("Your order: ", new_l)
 
 print("Actual order: ", l)
 
-if new_l == l[0:N]:
+if new_l == l[0:maxlen]:
     print("Excellent !!")
 else:
     print("Oops !!")
